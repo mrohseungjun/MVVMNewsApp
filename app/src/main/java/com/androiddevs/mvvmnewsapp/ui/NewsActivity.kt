4cd7205f7960704784320428318e97d0a539d8ab
오승준 <dhtmdwns54@naver.com>
2023-09-07 23:43:13 +0900

    build: navigation-fragment, ui Update fix: activity_news: Error inflating class fragment

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -3,24 +3,39 @@ package com.androiddevs.mvvmnewsapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.databinding.ActivityNewsBinding
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
-import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityNewsBinding
     lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
 
-        val newsRepository = NewsRepository(ArticleDatabase(this))
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = obtainViewModel()
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+    }
+
+    private fun getDatabaseInstance(): ArticleDatabase {
+        return ArticleDatabase(this)
+    }
+
+    private fun obtainViewModel(): NewsViewModel {
+        val newsRepository = NewsRepository(getDatabaseInstance())
         val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
-        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
-
-        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+        return ViewModelProvider(this, viewModelProviderFactory)[NewsViewModel::class.java]
     }
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModelProviderFactory.kt
@@ -5,9 +5,9 @@ import androidx.lifecycle.ViewModelProvider
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 
 class NewsViewModelProviderFactory(
-    val newsRepository: NewsRepository
+    private val newsRepository: NewsRepository
 ) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return NewsViewModel(newsRepository) as T
     }
 }
